09825ccdee28caea801721d706d87e32c6210116
LIUQI <1139642444@qq.com>
2022-10-30 18:59:41 +0800

    第一次提交框架

---
+++ PracticeNotesForOralArithmetic/PracticeNotesForOralArithmetic/PracticeNotesForOralArithmeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeNotesForOralArithmeticApp.swift
+//  PracticeNotesForOralArithmetic
+//
+//  Created by 刘奇 on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct PracticeNotesForOralArithmeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
